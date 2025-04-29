9ebb3b5b3a43945385b4f18c4d839373452f67b6
shahbhavya7 <phantomthunder77@gmail.com>
2025-04-30 00:01:43 +0530

    init

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import {ReactNode} from 'react'
+
+const Rootlayout = ({children}:{children: ReactNode}) => { // we are using ReactNode type for children prop to allow any type of children including string, number, element, etc.
+  // children beacause we are using this layout for multiple pages and we want to render the children of the page inside this layout
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Rootlayout
