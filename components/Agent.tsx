ee51cadeafcd28848e079f46ee4816cf96d112e6
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2025-05-18 01:46:11 +0530

    Agent Mid

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,14 +1,22 @@
+"use client";
+import { vapi } from "@/lib/vapi.sdk";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import React from 'react'
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 enum CallStatus {
-    INACTIVE = "INACTIVE",
-    CONNECTING = "CONNECTING",
-    ACTIVE = "ACTIVE",
-    FINISHED = "FINISHED",
-    }
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
 
+interface SavedMessage {
+  role: "user" | "system" | "assistant";
+  content: string;
+}
 
 const Agent = ({
   userName,
@@ -18,20 +26,69 @@ const Agent = ({
   type,
   questions,
 }: AgentProps) => {
-    const callStatus = CallStatus.FINISHED; // Replace with actual call status
-    const isSpeaking = true; // Replace with actual state
-    const messages = [
-        'what is your name?',
-        'My name is John Doe , I am a software engineer with 5 years of experience.',
+  const router = useRouter();
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [lastMessage, setLastMessage] = useState<string>("");
 
-    ]; // Replace with actual messages
-    const lastMessage = messages[messages.length - 1]; // Replace with actual last message
+  useEffect(() => { // this is react hook which will work when the component is mounted i.e. when the component is rendered
+    const onCallStart = () => { // this function will be called when the call starts , it will set the call status to active
+      setCallStatus(CallStatus.ACTIVE);
+    };
+
+    const onCallEnd = () => { // this function will be called when the call ends , it will set the call status to finished
+      setCallStatus(CallStatus.FINISHED);
+    };
+
+    const onMessage = (message: Message) => { // this function will be called when the message is received , it will set the last message to the message received
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [...prev, newMessage]);
+      }
+    };
+
+    const onSpeechStart = () => { // this function will be called when the speech starts , it will set the is speaking to true
+      console.log("speech start");
+      setIsSpeaking(true);
+    };
+
+    const onSpeechEnd = () => { // this function will be called when the speech ends , it will set the is speaking to false
+      console.log("speech end");
+      setIsSpeaking(false);
+    };
+
+    const onError = (error: Error) => { // this function will be called when there is an error , it will log the error
+      console.log("Error:", error);
+    };
+
+    // now we are forwarding the events to the functions we created above to vapi 
+    vapi.on("call-start", onCallStart); 
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+
+    return () => { // this function will be called when the component is unmounted , it will remove the event listeners , so that we dont have memory leaks
+      // for next time we will not have the same event listeners
+      // this is important because if we dont remove the event listeners , it will keep listening to the events and will cause memory leaks
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
+
+  
   return (
     <>
-    <div className="call-view">
+      <div className="call-view">
         <div className="card-interviewer">
-            <div className="avatar">
-                <Image
+          <div className="avatar">
+            <Image
               src="/ai-avatar.png"
               alt="profile-image"
               width={65}
@@ -39,12 +96,12 @@ const Agent = ({
               className="object-cover"
             />
             {isSpeaking && <span className="animate-speak" />}
-            </div>
-             <h3>AI Interviewer</h3>
+          </div>
+          <h3>AI Interviewer</h3>
         </div >
         <div className="card-border">
-            <div className="card-content">
-                 <Image
+          <div className="card-content">
+            <Image
               src="/user-avatar.png"
               alt="profile-image"
               width={539}
@@ -52,11 +109,11 @@ const Agent = ({
               className="rounded-full object-cover size-[120px]"
             />
             <h3>{userName}</h3>
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
 
-          {messages.length > 0 && (
+      {messages.length > 0 && (
         <div className="transcript-border">
           <div className="transcript">
             <p
@@ -72,26 +129,26 @@ const Agent = ({
         </div>
       )}
 
-    <div className='w-full flex justify-center'>
+      <div className='w-full flex justify-center'>
         {callStatus !== "ACTIVE" ? (
-            <button className='relative btn-call'>
-                            <span
+          <button className='relative btn-call'>
+            <span
               className={cn(
                 "absolute animate-ping rounded-full opacity-75",
                 callStatus !== "CONNECTING" && "hidden"
               )}
             />
-                <span className='relative'>
-                    {callStatus === "INACTIVE" || callStatus === "FINISHED" ? 'Call' : ' . . .'}
-                </span>
-            </button>
-        ): (
-            <button className='btn-disconnect'>
-               END
-            </button>
+            <span className='relative'>
+              {callStatus === "INACTIVE" || callStatus === "FINISHED" ? 'Call' : ' . . .'}
+            </span>
+          </button>
+        ) : (
+          <button className='btn-disconnect'>
+            END
+          </button>
         )}
-    </div>
-     </>   
+      </div>
+    </>
   )
 }
 
